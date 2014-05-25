0297aedc381417932b07c1330a550cdff2e25394
tuns calin <calin.tuns@live.com>
2014-05-25 17:33:53 +0200

    Reduce Collection

---
+++ Extensions/ListExtensionTests/ListExtensions.cs
@@ -0,0 +1,68 @@
+﻿using ListExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListExtensionTests
+{
+    [TestClass]
+    public class ListExtensions
+    {
+        [TestMethod]
+        public void ReduceShouldWork_ForLists()
+        {
+            var elem1 = new { x = 1, y = new List<string> { "a", "b" } };
+            var elem2 = new { x = 1, y = new List<string> { "c", "d" } };
+
+            var result = elem1.ReduceCollectionProperties(elem2);
+
+            Assert.AreEqual(1, result.x);
+            Assert.IsNotNull(result.y);
+            Assert.AreEqual(4, result.y.Count);
+            Assert.AreEqual("a", result.y.FirstOrDefault());
+            Assert.AreEqual("d", result.y.LastOrDefault());
+        }
+
+        [TestMethod]
+        public void ReduceShouldWork_ForArrays()
+        {
+            var elem1 = new ArrayItem { x = 1, y = new[] { "a", "b" } };
+            var elem2 = new ArrayItem { x = 1, y = new[] { "c", "d" } };
+
+            var result = elem1.ReduceCollectionProperties(elem2);
+
+            Assert.AreEqual(1, result.x);
+            Assert.IsNotNull(result.y);
+            Assert.AreEqual(4, result.y.Length);
+            Assert.AreEqual("a", result.y.FirstOrDefault());
+            Assert.AreEqual("d", result.y.LastOrDefault());
+        }
+
+        [TestMethod]
+        public void ReduceShouldWork_ForDictionaries()
+        {
+            var elem1 = new DictionaryItem { x = 1, y = new Dictionary<string, string> { { "a", "b" } } };
+            var elem2 = new DictionaryItem { x = 1, y = new Dictionary<string, string> { { "c", "d" } } };
+
+            var result = elem1.ReduceCollectionProperties(elem2);
+
+            Assert.AreEqual(1, result.x);
+            Assert.IsNotNull(result.y);
+            Assert.AreEqual(2, result.y.Count);
+            Assert.AreEqual("a", result.y.Keys.FirstOrDefault());
+            Assert.AreEqual("d", result.y.Values.LastOrDefault());
+        }
+
+        class ArrayItem
+        {
+            public int x { get; set; }
+            public string[] y { get; set; }
+        }
+
+        class DictionaryItem
+        {
+            public int x { get; set; }
+            public Dictionary<string, string> y { get; set; }
+        }
+    }
+}
---
+++ Extensions/ListExtensions/ObjectExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ListExtensions
+{
+    public static class ObjectExtension
+    {
+        public static T ReduceCollectionProperties<T>(this T firstElement, T secondElement)
+            where T: class
+        {
+            var arrayType = typeof(Array);
+            var enumerableType = typeof(IEnumerable<>);
+
+            typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.GetField | BindingFlags.Static)
+                .ToList()
+                .ForEach(propInfo => {
+                    // if property is a List or a Dictionary
+                    if (propInfo.PropertyType.IsGenericType &&
+
+                        (propInfo.PropertyType).GenericTypeArguments != null && 
+                        (propInfo.PropertyType).GenericTypeArguments.Length > 0)
+                    {
+                        var genericArguments = (propInfo.PropertyType).GenericTypeArguments;
+                        var isListProperty = genericArguments.Count() == 1 && propInfo.PropertyType.IsEquivalentTo(typeof(List<>).MakeGenericType(genericArguments.First()));
+                   
+                        dynamic first = propInfo.GetValue(firstElement, null);
+                        dynamic second = propInfo.GetValue(secondElement, null);
+
+                        if (isListProperty)
+                        {
+                           first.AddRange(second);
+
+                            return;
+                        }
+
+                        var isDictionaryProperty = genericArguments.Count() == 2 && propInfo.PropertyType.IsEquivalentTo(typeof(Dictionary<,>).MakeGenericType(genericArguments));
+
+                        if (isDictionaryProperty)
+                        {
+                            foreach (var item in second)
+                            {
+                                // explicitly DON'T check to see if the key in the second dictionary is present or not in the first one
+                                // if the key is present, the Add operation should fail naturally
+                                first.Add(item.Key, item.Value);
+                            }
+
+                            return;
+                        }
+                    }
+
+                    // if property is an Array
+                    if (propInfo.PropertyType.IsArray)
+                    {
+                        dynamic firstArray = propInfo.GetValue(firstElement, null);
+                        dynamic secondArray = propInfo.GetValue(secondElement, null);
+
+                        var newArray = Array.CreateInstance(propInfo.PropertyType.GetElementType(), firstArray.Length + secondArray.Length);
+                        for (var i = 0; i < firstArray.Length; i++)
+                        {
+                            newArray[i] = firstArray[i];
+                        }
+                        for (var i = 0; i < secondArray.Length; i++)
+                        {
+                            newArray[firstArray.Length + i] = secondArray[i];
+                        }
+
+                        propInfo.SetValue(firstElement, newArray);
+                    }
+                });
+
+            return firstElement;
+        }
+    }
+}
